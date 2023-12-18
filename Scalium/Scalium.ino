808c0bf656df89e7287764de0bcd5b268c4683d5
BohdanRomaniuk <bohdan2307@gmail.com>
2023-12-19 00:32:30 +0200

    Initial project created

---
+++ Scalium/Scalium.ino
@@ -0,0 +1,23 @@
+#include <FastLED.h>
+
+#define STEPS_COUNT 14
+#define LED_COUNT_PER_STEP 36
+
+#define CURRENT_LIMIT 10000
+#define BRIGHTNESS 255
+#define NIGHT_BRIGHTNESS 255
+
+#define BOTTOM_SENSOR_PIN 1
+#define UPPER_SENSOR_PIN 2
+#define LED_PIN 6
+
+const int ledCount = STEPS_COUNT*LED_COUNT_PER_STEP;
+
+void setup() {
+  
+  
+}
+
+void loop() {
+  
+}
